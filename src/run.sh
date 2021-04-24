18633c0e070ae4af61b7b58643d6405843858c4a
Dirk Bosman <dirk.bosman@oetkerdigital.com>
2021-04-24 22:56:35 +0200

    pytest code on chapters updated

--- src/run.sh
+++ src/run.sh
@@ -1,6 +1,6 @@
 pytest --help
-#pytest -v test_one.py
-#pytest -v test_two.py
+#cd ch1 && pytest -v test_one.py
+#cd ch1 && pytest -v test_two.py
 #cd ch1/tasks && pytest -v test_three.py
 #cd ch1/tasks && pytest -v test_four.py
 #cd ch1/tasks && pytest -v # overall test run
